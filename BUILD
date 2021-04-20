609a794501f3ee6a57accd98d93368a8e206d91d
Luca Milanesio <luca.milanesio@gmail.com>
2021-04-20 12:34:21 +0100

    Initial stable-2.16 commit from kafka-events repo
    Change-Id: Ib67b0c7a3744bc7edd12c79e765cba85bd6974d1

---
+++ BUILD
@@ -0,0 +1,48 @@
+load("//tools/bzl:junit.bzl", "junit_tests")
+load(
+    "//tools/bzl:plugin.bzl",
+    "PLUGIN_DEPS",
+    "PLUGIN_TEST_DEPS",
+    "gerrit_plugin",
+)
+
+gerrit_plugin(
+    name = "events-kafka",
+    srcs = glob(["src/main/java/**/*.java"]),
+    manifest_entries = [
+        "Gerrit-PluginName: events-kafka",
+        "Gerrit-Module: com.googlesource.gerrit.plugins.kafka.Module",
+        "Implementation-Title: Gerrit Apache Kafka plugin",
+        "Implementation-URL: https://gerrit.googlesource.com/plugins/events-kafka",
+    ],
+    resources = glob(["src/main/resources/**/*"]),
+    deps = [
+        "@kafka_client//jar",
+    ],
+)
+
+junit_tests(
+    name = "events_kafka_tests",
+    srcs = glob(["src/test/java/**/*.java"]),
+    tags = ["events-kafka"],
+    deps = [
+        ":events-kafka__plugin_test_deps",
+        "//lib/testcontainers",
+        "@kafka_client//jar",
+        "@testcontainers-kafka//jar",
+    ],
+)
+
+java_library(
+    name = "events-kafka__plugin_test_deps",
+    testonly = 1,
+    visibility = ["//visibility:public"],
+    exports = PLUGIN_DEPS + PLUGIN_TEST_DEPS + [
+        ":events-kafka__plugin",
+        "@testcontainers-kafka//jar",
+        "//lib/jackson:jackson-annotations",
+        "//lib/testcontainers",
+        "//lib/testcontainers:docker-java-api",
+        "//lib/testcontainers:docker-java-transport",
+    ],
+)
---
+++ src/main/java/com/googlesource/gerrit/plugins/kafka/session/KafkaSession.java
@@ -0,0 +1,75 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.kafka.session;
+
+import com.google.inject.Inject;
+import com.googlesource.gerrit.plugins.kafka.config.KafkaProperties;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class KafkaSession {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(KafkaSession.class);
+  private final KafkaProperties properties;
+  private volatile Producer<String, String> producer;
+
+  @Inject
+  public KafkaSession(KafkaProperties properties) {
+    this.properties = properties;
+  }
+
+  public boolean isOpen() {
+    if (producer != null) {
+      return true;
+    }
+    return false;
+  }
+
+  public void connect() {
+    if (isOpen()) {
+      LOGGER.debug("Already connected.");
+      return;
+    }
+
+    LOGGER.info("Connect to {}...", properties.getProperty("bootstrap.servers"));
+    /* Need to make sure that the thread of the running connection uses
+     * the correct class loader otherwize you can endup with hard to debug
+     * ClassNotFoundExceptions
+     */
+    setConnectionClassLoader();
+    producer = new KafkaProducer<>(properties);
+    LOGGER.info("Connection established.");
+  }
+
+  private void setConnectionClassLoader() {
+    Thread.currentThread().setContextClassLoader(KafkaSession.class.getClassLoader());
+  }
+
+  public void disconnect() {
+    LOGGER.info("Disconnecting...");
+    if (producer != null) {
+      LOGGER.info("Closing Producer {}...", producer);
+      producer.close();
+    }
+    producer = null;
+  }
+
+  public void publish(String messageBody) {
+    producer.send(new ProducerRecord<>(properties.getTopic(), "" + System.nanoTime(), messageBody));
+  }
+}
