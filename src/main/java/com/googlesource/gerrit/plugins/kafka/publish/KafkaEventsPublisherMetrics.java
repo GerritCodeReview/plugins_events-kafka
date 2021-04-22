6df4bb61ba3fb7bf2db279ec75f3831527261659
Marcin Czech <maczech@gmail.com>
2021-04-22 07:53:01 +0200

    Fix compilation issue
    After renaming the repository some code was not updated to branch 3.3
    
    Change-Id: If58ead7fcbd8d564f54d7bc1d7d779970104d244

--- src/main/java/com/googlesource/gerrit/plugins/kafka/publish/KafkaEventsPublisherMetrics.java
+++ src/main/java/com/googlesource/gerrit/plugins/kafka/publish/KafkaEventsPublisherMetrics.java
@@ -11,41 +11,38 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 package com.googlesource.gerrit.plugins.kafka.publish;
 
 import com.google.gerrit.metrics.Counter1;
 import com.google.gerrit.metrics.Description;
-import com.google.gerrit.metrics.Field;
 import com.google.gerrit.metrics.MetricMaker;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import com.googlesource.gerrit.plugins.kafka.KafkaEventsMetrics;
 
 @Singleton
-public class KafkaEventsPublisherMetrics {
+public class KafkaEventsPublisherMetrics extends KafkaEventsMetrics {
   private static final String PUBLISHER_SUCCESS_COUNTER = "broker_msg_publisher_success_counter";
   private static final String PUBLISHER_FAILURE_COUNTER = "broker_msg_publisher_failure_counter";
-
   private final Counter1<String> brokerPublisherSuccessCounter;
   private final Counter1<String> brokerPublisherFailureCounter;
 
   @Inject
   public KafkaEventsPublisherMetrics(MetricMaker metricMaker) {
-
     this.brokerPublisherSuccessCounter =
         metricMaker.newCounter(
             "kafka/broker/broker_message_publisher_counter",
             new Description("Number of successfully published messages by the broker publisher")
                 .setRate()
                 .setUnit("messages"),
-            Field.ofString(PUBLISHER_SUCCESS_COUNTER, "Broker message published count"));
+            stringField(PUBLISHER_SUCCESS_COUNTER, "Broker message published count"));
     this.brokerPublisherFailureCounter =
         metricMaker.newCounter(
             "kafka/broker/broker_message_publisher_failure_counter",
             new Description("Number of messages failed to publish by the broker publisher")
                 .setRate()
                 .setUnit("errors"),
-            Field.ofString(PUBLISHER_FAILURE_COUNTER, "Broker failed to publish message count"));
+            stringField(PUBLISHER_FAILURE_COUNTER, "Broker failed to publish message count"));
   }
 
   public void incrementBrokerPublishedMessage() {
