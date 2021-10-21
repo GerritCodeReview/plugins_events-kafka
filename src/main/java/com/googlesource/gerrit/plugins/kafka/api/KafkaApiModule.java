2425bf0bf37ca79dc5103b630035a93d94cb32ff
Luca Milanesio <luca.milanesio@gmail.com>
2021-10-21 21:54:05 +0100

    Receive messages through Kafka REST API
    Allow to receive messages through the Confluent REST Proxy
    instead of using the Kafka native client.
    
    Change-Id: I30142b88d8a215dc61261877cb8efe5d246320ff

--- src/main/java/com/googlesource/gerrit/plugins/kafka/api/KafkaApiModule.java
+++ src/main/java/com/googlesource/gerrit/plugins/kafka/api/KafkaApiModule.java
@@ -26,9 +26,11 @@ import com.google.inject.Scopes;
 import com.google.inject.Singleton;
 import com.google.inject.TypeLiteral;
 import com.googlesource.gerrit.plugins.kafka.broker.ConsumerExecutor;
+import com.googlesource.gerrit.plugins.kafka.config.KafkaProperties.ClientType;
 import com.googlesource.gerrit.plugins.kafka.config.KafkaSubscriberProperties;
 import com.googlesource.gerrit.plugins.kafka.subscribe.KafkaEventDeserializer;
 import com.googlesource.gerrit.plugins.kafka.subscribe.KafkaEventNativeSubscriber;
+import com.googlesource.gerrit.plugins.kafka.subscribe.KafkaEventRestSubscriber;
 import com.googlesource.gerrit.plugins.kafka.subscribe.KafkaEventSubscriber;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
@@ -56,7 +58,17 @@ public class KafkaApiModule extends LifecycleModule {
 
   @Override
   protected void configure() {
-    bind(KafkaEventSubscriber.class).to(KafkaEventNativeSubscriber.class);
+    ClientType clientType = configuration.getClientType();
+    switch (clientType) {
+      case NATIVE:
+        bind(KafkaEventSubscriber.class).to(KafkaEventNativeSubscriber.class);
+        break;
+      case REST:
+        bind(KafkaEventSubscriber.class).to(KafkaEventRestSubscriber.class);
+        break;
+      default:
+        throw new IllegalArgumentException("Unsupport Kafka client type " + clientType);
+    }
 
     bind(ExecutorService.class)
         .annotatedWith(ConsumerExecutor.class)
