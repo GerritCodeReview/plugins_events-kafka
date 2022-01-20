71cc8b6b319e6af107f7309865d47847759a8683
Marcin Czech <maczech@gmail.com>
2022-01-20 01:49:50 +0100

    Use Kafka REST Proxy id to subscribe to the correct instance
    When using a Kafka REST Proxy with multiple instances behind a
    workload balancer, the consumers are allocated on one of the
    allocated instances. The Kafka REST client needs to understand
    which one is the instance that the client should talk to.
    
    For deployments that can use workload balancers with sticky
    sessions, there isn't any logic needed on the client side, because
    the cookies allocated and stored on the client would do the job.
    However, when using a standard K8s deployment, the sticky session
    allocation isn't an option and any of the incoming traffic may
    reach any of the pods.
    
    Kafka REST Proxy allows with this change to specify the REST_PROXY_ID
    with the placeholder ${rest_proxy_id} in the API URL, to help
    redirecting the calls to the correct instance.
    
    Example:
    [plugin "events-kafka"]
      restApiUri = http://kafka-rest:8080/${kafka_rest_id}
    
    Change-Id: I396c3bc759ee05547e8c16cb4806b4fea63b833c

--- src/main/java/com/googlesource/gerrit/plugins/kafka/subscribe/KafkaEventRestSubscriber.java
+++ src/main/java/com/googlesource/gerrit/plugins/kafka/subscribe/KafkaEventRestSubscriber.java
@@ -61,6 +61,9 @@ import org.apache.kafka.common.serialization.Deserializer;
 public class KafkaEventRestSubscriber implements KafkaEventSubscriber {
   private static final FluentLogger logger = FluentLogger.forEnclosingClass();
   private static final int DELAY_RECONNECT_AFTER_FAILURE_MSEC = 1000;
+  private static final int INSTANCE_ID_PREFIX_LEN = "rest-consumer-".length();
+  private static final int INSTANCE_ID_SUFFIX_LEN =
+      "-9836fe85-d838-4722-97c9-4a7b-34e834d".length();
 
   private final OneOffRequestContext oneOffCtx;
   private final AtomicBoolean closed = new AtomicBoolean(false);
@@ -281,10 +284,24 @@ public class KafkaEventRestSubscriber implements KafkaEventSubscriber {
           new InputStreamReader(response.getEntity().getContent(), StandardCharsets.UTF_8)) {
         JsonObject responseJson = gson.fromJson(bodyReader, JsonObject.class);
         URI consumerUri = new URI(responseJson.get("base_uri").getAsString());
-        return Futures.immediateFuture(restClient.resolveURI(consumerUri.getPath()));
+        String instanceId = responseJson.get("instance_id").getAsString();
+
+        String restProxyId = getRestProxyId(instanceId);
+        return Futures.immediateFuture(
+            restClient.resolveKafkaRestApiUri(restProxyId, consumerUri.getPath()));
       } catch (UnsupportedOperationException | IOException | URISyntaxException e) {
         return Futures.immediateFailedFuture(e);
       }
+    }
+
+    private String getRestProxyId(String instanceId) {
+      int instanceIdLen = instanceId.length();
+      if (instanceIdLen <= INSTANCE_ID_SUFFIX_LEN + INSTANCE_ID_PREFIX_LEN) {
+        return "";
+      }
+
+      return instanceId.substring(
+          INSTANCE_ID_PREFIX_LEN, instanceId.length() - INSTANCE_ID_SUFFIX_LEN);
     }
 
     private ListenableFuture<ConsumerRecords<byte[], byte[]>> convertRecords(
